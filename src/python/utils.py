084a71222f8933f3059872bee7c55dd5e80801e6
VERET Salif <salif.veret@telecomnancy.eu>
2026-02-18 00:03:18 +0100

    fix: changed format structure for easier handle later

--- src/python/utils.py
+++ src/python/utils.py
@@ -1,15 +1,27 @@
-
+"""Utils for formatting input file containing tasks"""
 
 def format_task(line: list) -> list:
-    """Format the task list to a [pid, arrival_time, nice, [calc_times], [I/O times]] format."""
-    nline = [line[i] for i in range(2)]
-    calcs = []
-    io = []
-    for i in range(2, len(line) - 1): #gather CPU tasks and I/O tasks together
-        if i%2 == 0:
-            calcs.append(line[i])
+    """Format the task list to a [pid, arrival_time, [(task_type, time), ...]] format."""
+    nline = [line[i] for i in range(3)]
+    tasks = []
+    for i in range(3, len(line)):  # gather CPU and I/O tasks as tuples
+        if i % 2 == 0:
+            tasks.append(("CPU", line[i]))
         else:
-            io.append(line[i])
+            tasks.append(("IO", line[i]))
 
-    nline = nline + [calcs] + [io]
+    nline = nline + [tasks]
     return nline
+
+def file_to_tasks(filename: str) -> list:
+    """"""
+    f = open(filename, "r")
+    lines = f.readlines()
+
+    f_lines = []
+    for line in lines:
+        s_line = line.strip("\n").split()
+        str_to_int_line = [s_line[0]] + [int(s_line[i]) for i in range(1, len(s_line))]
+        f_lines.append(format_task(str_to_int_line))
+
+    return f_lines
