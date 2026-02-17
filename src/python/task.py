39d1211903833bde371bdaa1e83853f381679ec3
VERET Salif <salif.veret@telecomnancy.eu>
2026-02-17 23:23:46 +0100

    feat: implement Task class for tasks representation

---
+++ src/python/task.py
@@ -0,0 +1,16 @@
+"""Task representation for SimpleCFS"""
+
+class Task:
+    def __init__(self, task_id: str, task_nice: int, arrival_time: float, bursts: list[tuple[float, int]]):
+        self.id: str = task_id
+        self.nice: int = task_nice
+        self.vruntime: float = 0.0
+        self.state: (None | str) = None
+        self.arrival_time = arrival_time
+        self.bursts = bursts #burst ex: ("CPU", 9)
+        self.current_burst: int = 0
+
+    def is_finished(self):
+        return self.current_burst == len(self.bursts)
+
+    
