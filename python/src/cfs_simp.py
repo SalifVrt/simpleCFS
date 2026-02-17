a242fc44ca248e9e852b29bb28078557c8858374
VERET Salif <salif.veret@telecomnancy.eu>
2026-02-18 00:59:23 +0100

    fix: forgot some files to track for changed project structure

---
+++ python/src/cfs_simp.py
@@ -0,0 +1,24 @@
+#import numpy as np
+#import utils
+
+MIN_GRANULARITY = 0.75  #granularité minimale
+L = 6   #période d'ordonnancement
+PRIO_TO_WEIGHT = [88761, 71755, 56483, 46273, 36291, 29154, 23254, 18705, 14949, 11916,
+                  9548, 7620, 6100, 4904, 3906,
+                  3121, 2501, 1991, 1586, 1277,
+                  1024, 820, 655, 526, 423, 335, 272, 215, 172, 137,
+                  110, 87, 70, 56, 45, 36, 29, 23, 18, 15]
+
+
+def cfs_simp(tasks: list[int | list[int]], L: int=L, min_granularity: float=MIN_GRANULARITY) -> None :
+    """
+    Simplified CFS scheduler.
+    
+    tasks = list of formatted tasks (see format_task())
+    L: int = Scheduling Period (in ms)
+    min_granularity: int = Minimal Granluarity (in ms)
+    """
+
+
+
+    pass
---
+++ python/src/task.py
@@ -0,0 +1,16 @@
+"""Task representation for simpleCFS"""
+
+class Task:
+    def __init__(self, task_id: str, arrival_time: float, task_nice: int, bursts: list[tuple]):
+        self.id: str = task_id
+        self.nice: int = task_nice
+        self.vruntime: float = 0.0
+        self.state: (None | str) = None
+        self.arrival_time = arrival_time
+        self.bursts = bursts #burst ex: ("CPU", 9)
+        self.current_burst: int = 0
+
+    def is_finished(self):
+        return self.current_burst == len(self.bursts)
+
+    
