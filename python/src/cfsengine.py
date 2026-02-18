accc58c67a6ff09e342717e4aff3828523957ccb
VERET Salif <salif.veret@telecomnancy.eu>
2026-02-18 09:54:56 +0100

    feat: CFSEngine init

--- python/src/cfsengine.py
+++ python/src/cfsengine.py
@@ -1,8 +1,13 @@
 """simpleCFS Engine."""
 
+from . import runqueue
+from . import task
 
 class CFSEngine:
-    
+    def __init__(self, tasks:list[task.Task]=[]):
+        self.rqueue = runqueue.Runqueue()
+        self.tasks = tasks
+        self.time = 0.0
 
     def run(self):
         pass
