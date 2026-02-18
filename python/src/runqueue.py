e6b725d640534c53686389ae175767d302e76516
VERET Salif <salif.veret@telecomnancy.eu>
2026-02-18 09:06:00 +0100

    feat: implement pick_next_task() method and changed get_min_vruntime() method

--- python/src/runqueue.py
+++ python/src/runqueue.py
@@ -1,14 +1,26 @@
 """Runqueue implementation for simpleCFS"""
 
 from operator import attrgetter
-from task import Task
+from . import task
 
 class Runqueue:
     def __init__(self):
         self.tasks = []
 
     def get_min_vruntime(self):
-        return min(self.tasks, key=attrgetter('vruntime'), default=None)
+        """Returns minimum vruntime and the task associated."""
+        return min(enumerate(self.tasks), key=lambda enum_pair: enum_pair[1].vruntime, default=None)
+    
+    def get_total_weight_from_queue(self):
+        return sum(map(task.Task.get_task_weight, self.tasks))
 
-    def add_task(self, task: Task):
+    def add_task(self, task: task.Task):
         pass
+
+    def pick_next_task(self):
+        """Picks the next task to execute in the runqueue."""
+        min = self.get_min_vruntime()
+
+        if not min:
+            return None
+        return min[0]
