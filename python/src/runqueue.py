417b4f4c6ea0c24d5fe99d37198fc24c5bd45362
VERET Salif <salif.veret@telecomnancy.eu>
2026-02-19 12:57:23 +0100

    feat: implement add_task() method in Runqueue class

--- python/src/runqueue.py
+++ python/src/runqueue.py
@@ -8,19 +8,26 @@ class Runqueue:
         self.tasks = []
 
     def get_min_vruntime(self):
-        """Returns minimum vruntime and the task associated."""
+        """Return minimum vruntime and the task associated."""
         return min(enumerate(self.tasks), key=lambda enum_pair: enum_pair[1].vruntime, default=None)
     
     def get_total_weight_from_queue(self):
+        """Return sum of the weights of all the tasks in the runqueue."""
         return sum(map(task.Task.get_task_weight, self.tasks))
 
     def add_task(self, task: task.Task):
-        pass
+        """Add a new task to runqueue."""
+
+        min_vruntime = self.get_min_vruntime()
+        if min_vruntime:
+            task.vruntime = max(min_vruntime[1], task.vruntime)
+        self.tasks.append(task)
+
 
     def pick_next_task(self):
-        """Picks the next task to execute in the runqueue."""
-        min = self.get_min_vruntime()
+        """Pick the next task to execute in the runqueue."""
 
+        min = self.get_min_vruntime()
         if not min:
             return None
         return min[0]
